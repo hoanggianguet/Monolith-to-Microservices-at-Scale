f80d9fd32f7d00e636be5d70fac9d0a327b16279
hoanggianguet <gianghoang.uet.vnu@gmail.com>
2023-06-15 04:29:21 +0700

    Updace config

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a700f0398694a468b9a7ecbdf083b44c-451318612.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0fc65096253a44b09119a97ef011907-629876839.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
